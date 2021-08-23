da3cd1facded67b04d4a4b223166695de22e9543
SIMDAV20 <david14847@gmail.com>
2021-08-23 18:56:38 -0500

    ruta padre dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,14 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot( routes ),
-    PagesRoutingModule,
+    PagesRoutingModule, 
     AuthRoutingModule
   ],
   exports: [RouterModule]
