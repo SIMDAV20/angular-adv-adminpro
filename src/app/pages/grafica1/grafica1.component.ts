bfd6993f47f58b3ffa9ec8d7efe8474d8ad3c492
SIMDAV20 <david14847@gmail.com>
2021-08-23 22:14:23 -0500

    aprendiendo a usar comp reutiolizables

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica1',
@@ -7,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class Grafica1Component implements OnInit {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+  ];
+  // public doughnutChartType: ChartType = 'doughnut';
+  public colors: Color[] = [
+    { backgroundColor: ['#9E120E', '#FF5800', '#FFB414']}
+  ];
 
   constructor() { }
 
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,10 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: [
+    './progress.component.css'
   ]
 })
 export class ProgressComponent implements OnInit {
+
+  progreso1: number = 25;
+  progreso2: number = 25;
+
+  get getProceso1() {
+    return `${ this.progreso1 }%`;
+  }
+
+  get getProceso2() {
+    return `${ this.progreso2 }%`;
+  }
+
+  cambioValorHijo( valor: number) {
+    console.log('hey', valor);
+  }
 
   constructor() { }
 
